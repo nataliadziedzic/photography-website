13cb01dffa117c1212ca06b0fe1a0e80ee615824
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-09-17 18:54:19 +0200

    Create Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,85 @@
 import React from 'react';
+import Navigation from "./Navigation";
+import { MenuBtn as Close, Stripe } from "../styles/Navigation-style";
+import { GalleryWrapper as Wrapper, ImagesContainer, NameInGallery, GoUp, ImageBox, Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12 } from "../styles/Gallery-styles";
+import hamburg from "../assets/img/hamburg-dark.jpg";
+import mother from "../assets/img/baby-mother.jpg";
+import berlin from "../assets/img/berlin.jpg";
+import parachute from "../assets/img/parachute-sky.jpg";
+import couple from "../assets/img/couple.jpg";
+import edge from "../assets/img/edge-legs.jpg";
+import jump from "../assets/img/jump.jpg";
+import factory from "../assets/img/factory.jpg";
+import camera from "../assets/img/camera-person.jpg";
+import puppy from "../assets/img/puppy.jpg";
+import girls from "../assets/img/girls.jpg";
+import guitarist from "../assets/img/guitarist.jpg";
 
 function Gallery() {
+    window.onscroll = () => { onScrollFunction() };
+
+    const onScrollFunction = () => {
+        const btnWrapper = document.querySelector(".btnWrapper");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            btnWrapper.style.display = "block";
+        } else {
+            btnWrapper.style.display = "none";
+        }
+    }
+    const topFunction = () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    const showImage = (src) => {
+        const div = document.querySelector(".imageBox");
+        const img = document.createElement("img");
+        img.style.width = "90%";
+        img.style.position = "fixed";
+        img.style.top = "50%";
+        img.style.left = "50%";
+        img.style.transform = "translate(-50%, -50%)";
+        img.setAttribute("src", src);
+        div.appendChild(img);
+        div.style.display = "block";
+        div.style.animation = "fadein .3s";
+    }
+    const closeBox = () => {
+        const div = document.querySelector(".imageBox");
+        div.style.animation = "fadeout .4s";
+        setTimeout(() => {
+            div.style.display = "none";
+            div.querySelector("img").remove();
+        }, 400)
+    }
     return (
-        <div>Gallery</div>
+        <>
+            <Navigation />
+            <Wrapper>
+                <ImagesContainer>
+                    <Image1 src={hamburg} alt="hamburg" onClick={() => showImage(hamburg)} />
+                    <Image2 src={mother} alt="mother" onClick={() => showImage(mother)} />
+                    <Image3 src={berlin} alt="berlin" onClick={() => showImage(berlin)} />
+                    <Image4 src={parachute} alt="parachute" onClick={() => showImage(parachute)} />
+                    <Image5 src={couple} alt="couple" onClick={() => showImage(couple)} />
+                    <Image6 src={jump} alt="jump" onClick={() => showImage(jump)} />
+                    <Image7 src={edge} alt="edge" onClick={() => showImage(edge)} />
+                    <Image8 src={factory} alt="factory" onClick={() => showImage(factory)} />
+                    <Image9 src={camera} alt="camera" onClick={() => showImage(camera)} />
+                    <Image10 src={puppy} alt="puppy" onClick={() => showImage(puppy)} />
+                    <Image11 src={girls} alt="girls" onClick={() => showImage(girls)} />
+                    <Image12 src={guitarist} alt="guitarist" onClick={() => showImage(guitarist)} />
+                </ImagesContainer>
+                <NameInGallery>Yasin Clements</NameInGallery>
+                <GoUp className="btnWrapper" onClick={topFunction}><i className="fas fa-arrow-up"></i></GoUp>
+                <ImageBox className="imageBox" >
+                    <Close close onClick={closeBox}>
+                        CLOSE
+                    <Stripe close />
+                    </Close>
+                </ImageBox>
+            </Wrapper>
+        </>
     )
 }
 export default Gallery
---
+++ src/styles/Gallery-styles.js
@@ -0,0 +1,220 @@
+import styled from "styled-components";
+import { Name } from "../styles/HomePage-style"
+
+export const GalleryWrapper = styled.div`
+    min-height: 100vh;
+    overflow-x: hidden;
+    @media only screen and (orientation: landscape){
+        max-height: 100vh;
+        overflow-x: visible;
+        overflow-y: hidden;
+    }
+`
+export const ImagesContainer = styled.div`
+    display: grid;
+    margin-top: 70px;
+    align-items: center;
+    @media only screen and (orientation: landscape){
+        margin-top: 0;
+        grid-auto-flow: column;
+        grid-template-rows: 50vh 50vh;
+        grid-template-columns: repeat(12,50%);
+    }
+    }
+`
+export const Image1 = styled.img`
+    width: 100%;
+    grid-row: 1;
+    grid-column: 1;
+    @media only screen and (orientation: landscape){
+        grid-row: 1/3;
+        grid-column: 1/2;
+        justify-self: start;
+        width: 80%;
+        margin: 0 0 90px 15px;
+    }
+`
+export const Image2 = styled.img`
+    width: 100%;
+    grid-row: 2;
+    grid-column: 2;
+    @media only screen and (orientation: landscape){
+        grid-row: 2;
+        grid-column: 2/3;
+        justify-self: start;
+        align-self: end;
+        width: 60%;
+        margin-left: -40px;
+        padding-bottom: 10px;
+    }
+`
+export const Image3 = styled.img`
+    width: 100%;    
+    grid-row: 3;
+    grid-column: 1;
+    @media only screen and (orientation: landscape){
+        grid-row: 1;
+        grid-column: 2/3;
+        justify-self: end;
+        width: 80%;        
+    }
+`
+export const Image4 = styled.img`
+    width: 100%;
+    grid-row: 4;
+    grid-column: 2;
+    @media only screen and (orientation: landscape){
+        grid-row: 1;
+        grid-column: 3/4;
+        justify-self: center;
+        align-self: start;
+        width: 70%;
+    }
+    @media only screen and (orientation: landscape) and (min-width: 1024px){
+        align-self: center;
+        justify-self: end; 
+    }
+    @media only screen and (orientation: landscape) and (min-width: 1400px){
+        align-self: start;
+        width: 60%;
+        margin-top: 20px; 
+    }
+
+`
+export const Image5 = styled.img`
+    width: 100%;
+    grid-row: 5;
+    grid-column: 1;
+    @media only screen and (orientation: landscape){
+        grid-row: 2/3;
+        grid-column: 3/4;
+        justify-self: start;
+        width: 100%;
+        margin-left: 115px;
+    }
+`
+export const Image6 = styled.img`
+    width: 100%;
+    grid-row: 6;
+    grid-column: 2;
+    @media only screen and (orientation: landscape){
+        grid-row: 1/3;
+        grid-column: 4/5;
+        justify-self: end;
+        width: 60%;
+    }
+`
+export const Image7 = styled.img`
+    width: 100%; 
+    grid-row: 7;
+    grid-column: 1;
+    @media only screen and (orientation: landscape){
+        grid-row: 1;
+        grid-column: 5/6;
+        width: 90%; 
+        justify-self: center;
+        margin-top: 100px;
+    }
+`
+export const Image8 = styled.img`
+    width: 100%;
+    grid-row: 8;
+    grid-column: 2;
+    @media only screen and (orientation: landscape){
+        grid-row: 1/3;
+        grid-column: 6/7;
+        justify-self: end;
+        width: 100%;
+    }
+`
+export const Image9 = styled.img` 
+    width: 100%;
+    grid-row: 9;
+    grid-column: 1;
+    @media only screen and (orientation: landscape){
+        grid-row: 1;
+        grid-column: 7/8;
+        justify-self: center;
+        width: 80%;
+    }
+`
+export const Image10 = styled.img`
+    width: 100%;
+    grid-row: 10;
+    grid-column: 2;
+    @media only screen and (orientation: landscape){
+        grid-row: 2;
+        grid-column: 7/8;
+        justify-self: end;
+        width: 70%;
+        margin-right: -100px;
+    }
+`
+export const Image11 = styled.img`
+    width: 100%;
+    grid-row: 11;
+    grid-column: 1;
+    @media only screen and (orientation: landscape){
+        grid-row: 1/3;
+        grid-column: 8/9;
+        justify-self: end;
+        width: 60%;
+    }
+    @media only screen and (orientation: landscape) and (min-width: 1024px){
+        margin-right: 50px;
+    }
+`
+export const Image12 = styled.img`
+    width: 80%;
+    justify-self: center;
+    padding: 25px 0;
+    grid-row: 12;
+    grid-column: 1/3;
+    @media only screen and (orientation: landscape){
+        grid-row: 2/3;
+        grid-column: 9/10;
+        justify-self: end;
+        width: 100%;
+        padding-left: 15px;
+    }
+`
+export const NameInGallery = styled(Name)`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    @media only screen and (min-width: 800px){
+        letter-spacing: 7px;
+    }
+`
+export const GoUp = styled.button`
+    display: none;
+    border-radius: 50%;
+    padding: 5px 7px;
+    margin: 10px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+`
+export const ImageBox = styled.div`
+    display: none;
+    width: 100%;
+    height: 100vh;
+    padding: 15px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: grey;
+    @keyframes fadein {
+        from { opacity: 0; }
+        to   { opacity: 1; }
+    }
+    @keyframes fadeout {
+        from { opacity: 1; }
+        to   { opacity: 0; }
+    }
+`
