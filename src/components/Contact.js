16be48a84dd7d7a593570984f702d385ad8d4de5
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-09-24 11:47:00 +0200

    Create Contact page with Formik

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,71 @@
 import React from 'react';
+import { Formik, useField } from 'formik';
+import * as Yup from "yup";
+import { Wrapper, ContactBox, Form, Intro, Error, Input, Message, Submit, SideImage } from "../styles/Contact-styles";
+import Navigation from "./Navigation"
+
+const MyInput = ({ ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <Input {...field} {...props} />
+            {meta.error && meta.touched && <Error>{meta.error}</Error>}
+        </>
+    );
+}
+const MyMessage = ({ ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <Message {...field} {...props} />
+            {meta.error && meta.touched && <Error>{meta.error}</Error>}
+        </>
+    );
+}
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <Wrapper>
+            <Navigation />
+            <ContactBox>
+                <SideImage />
+                <Formik
+                    initialValues={{
+                        topic: '',
+                        email: '',
+                        message: '',
+                    }}
+                    validationSchema={Yup.object({
+                        topic: Yup.string()
+                            .min(5, 'Must be at least 5 characters.')
+                            .max(15, "It's too long!")
+                            .required('Required.'),
+                        email: Yup.string()
+                            .email('Invalid email address')
+                            .required('Required.'),
+                        message: Yup.string()
+                            .min(10, 'Your message is too short :(')
+                            .required('Required.'),
+                    })}
+                    onSubmit={(values, { setSubmitting, resetForm }) => {
+                        setTimeout(() => {
+                            alert("Thank you for your message!");
+                            resetForm();
+                            setSubmitting(false);
+                        }, 2000)
+                    }}
+                >{props => (
+                    <Form onSubmit={props.handleSubmit}>
+                        <Intro>Write to me!</Intro>
+                        <MyInput placeholder="Topic" name="topic" />
+                        <MyInput type="email" name="email" placeholder="E-mail" />
+                        <MyMessage placeholder="Message..." name="message" />
+                        <Submit type="submit">{props.isSubmitting ? "Loading..." : "Submit!"}</Submit>
+                    </Form>
+                )}
+                </Formik>
+            </ContactBox>
+        </Wrapper>
     )
 }
 export default Contact
---
+++ src/styles/Contact-styles.js
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+import sideImage from "../assets/img/sideImage.jpg"
+
+export const Wrapper = styled.div`
+    height: 100vh;
+    overflow: hidden;
+    background: rgb(78,60,61);
+    background: linear-gradient(0deg, rgba(78,60,61,1) 0%, rgba(61,78,60,1) 100%);  
+`
+export const ContactBox = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 90vw;
+    height: 80vh;
+    background-color: #000;
+    display: flex;
+    flex-direction: column;
+    border-radius: 5%;
+    border: 1px solid black;
+    @media only screen and (orientation: landscape){
+        width: 60vw;
+        height: 90vh;
+        flex-direction: row;
+    }
+    @media only screen and (orientation: landscape) and (min-width: 1024px){
+        width: 85vw;
+    }
+    @media only screen and (orientation: landscape) and (min-width: 1400px){
+        width: 60vw;
+    }
+`
+export const Form = styled.form`
+    width: 100%;
+    height: 50%;
+    max-width: 800px;
+    text-align: center; 
+    margin-top: 40px; 
+    overflow: auto; 
+    @media only screen and (orientation: landscape){
+        width: 50%;
+        height: auto;
+    }
+`
+export const Intro = styled.h1`
+    margin-bottom: 50px;
+    font-family: 'Lato';
+    font-size: 26px;
+`
+export const Error = styled.div`
+    color: red;
+    font-size: 10px;
+    padding: 5px;
+    margin-top: -40px;    
+    margin-bottom: 18px;    
+`
+export const Input = styled.input`
+height: 40px;
+min-height: 25px;
+width: 70%;
+margin-bottom: 50px;
+background-color: transparent;
+border: none;
+border-bottom: 2px solid #ccc;
+font-size: 15px;
+`
+export const Message = styled.textarea`
+    height: 40%;
+    min-height: 100px;
+    width: 70%;
+    padding: 10px 20px; 
+    background-color: transparent;
+    border: none;
+    border-bottom: 1px solid #ccc;
+`
+export const Submit = styled.button`
+    width: 50%;
+    max-width: 200px;
+    height: 45px;
+    margin: 35px 20px 20px;
+    background-color: #711d18;
+    border: none;
+    border-radius: 12px;
+`
+export const SideImage = styled.div`
+    background: url(${sideImage});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 50%;
+    @media only screen and (orientation: landscape){
+        width: 50%;
+        height: auto;
+    }
+`
