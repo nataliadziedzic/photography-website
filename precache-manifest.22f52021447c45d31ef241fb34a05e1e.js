b3fe7638332aae5d835eacd2d2eb0fb52077765f
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-09-29 22:01:31 +0200

    Updates

---
+++ precache-manifest.22f52021447c45d31ef241fb34a05e1e.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfc4849804836c768e23bbad01df754",
+    "url": "/photography-website/index.html"
+  },
+  {
+    "revision": "dae9d0456bee1714ff84",
+    "url": "/photography-website/static/js/2.e214d0bb.chunk.js"
+  },
+  {
+    "revision": "e1736599151d6ad7ec1d8eaf122b7074",
+    "url": "/photography-website/static/js/2.e214d0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a5061c544e98d1a5ce",
+    "url": "/photography-website/static/js/main.63459281.chunk.js"
+  },
+  {
+    "revision": "fa1fe7e40c664ab585d4",
+    "url": "/photography-website/static/js/runtime-main.446dae6c.js"
+  },
+  {
+    "revision": "9c805d07948b5704806ee9b7759f5d54",
+    "url": "/photography-website/static/media/author.9c805d07.jpg"
+  },
+  {
+    "revision": "f2bab6a4f9ee4d8e9d0bfe50885d1074",
+    "url": "/photography-website/static/media/baby-mother.f2bab6a4.jpg"
+  },
+  {
+    "revision": "3fd8cca00b3ed08356cf8f7e13ab3668",
+    "url": "/photography-website/static/media/background-gallery.3fd8cca0.jpg"
+  },
+  {
+    "revision": "14a2cad35c3059e1b372bcde5cb12614",
+    "url": "/photography-website/static/media/berlin.14a2cad3.jpg"
+  },
+  {
+    "revision": "1c110e7a2f062eb4c1dc06471a05e33b",
+    "url": "/photography-website/static/media/business.1c110e7a.jpg"
+  },
+  {
+    "revision": "450dcab515f1ea1d1c7ad24b88e47d93",
+    "url": "/photography-website/static/media/camera-person.450dcab5.jpg"
+  },
+  {
+    "revision": "50454b559478b00d708b1f62b4444c78",
+    "url": "/photography-website/static/media/canon.50454b55.jpg"
+  },
+  {
+    "revision": "78bad011fc908fa898b98bb99da64d60",
+    "url": "/photography-website/static/media/communication.78bad011.jpg"
+  },
+  {
+    "revision": "252dd94347bf7f83ec4c43ab03bdc223",
+    "url": "/photography-website/static/media/contact.252dd943.jpg"
+  },
+  {
+    "revision": "27fd72811e7a5e2d214cf4dcbefe708b",
+    "url": "/photography-website/static/media/couple.27fd7281.jpg"
+  },
+  {
+    "revision": "b531f3d08bd09d6897d9d41bd6f4020a",
+    "url": "/photography-website/static/media/courses-top-picture.b531f3d0.jpg"
+  },
+  {
+    "revision": "124cb4220dd63c23cc19fd99cd4c2ea2",
+    "url": "/photography-website/static/media/courses.124cb422.jpg"
+  },
+  {
+    "revision": "32bf55b4f0fdf82626fdfd9a54f1e2a3",
+    "url": "/photography-website/static/media/edge-legs.32bf55b4.jpg"
+  },
+  {
+    "revision": "65efcc8e792c238226a613fe8e90ec77",
+    "url": "/photography-website/static/media/factory.65efcc8e.jpg"
+  },
+  {
+    "revision": "6feea754c9c1fde536d8930bc799802a",
+    "url": "/photography-website/static/media/gallery.6feea754.jpg"
+  },
+  {
+    "revision": "52c41dbd8a864207cedd3e25c254cd0f",
+    "url": "/photography-website/static/media/girls.52c41dbd.jpg"
+  },
+  {
+    "revision": "7143b1a158e5723079f6651e7c218959",
+    "url": "/photography-website/static/media/home.7143b1a1.jpg"
+  },
+  {
+    "revision": "aa5806b94a54c5cc70efa83f06742163",
+    "url": "/photography-website/static/media/japan.aa5806b9.jpg"
+  },
+  {
+    "revision": "d2cedf05841c9b1c5df2f90c2a149d25",
+    "url": "/photography-website/static/media/jump.d2cedf05.jpg"
+  },
+  {
+    "revision": "8bb2cc600dfcc394127728b7eef4a6d1",
+    "url": "/photography-website/static/media/outfits.8bb2cc60.jpg"
+  },
+  {
+    "revision": "33e30dc51da7e2e17af73930ee32de4a",
+    "url": "/photography-website/static/media/parachute-sky.33e30dc5.jpg"
+  },
+  {
+    "revision": "5b159cf64902aaf4085d655b4e29ecc6",
+    "url": "/photography-website/static/media/products.5b159cf6.jpg"
+  },
+  {
+    "revision": "a1e77e098bf3c1d1c4a499f36a2d1d30",
+    "url": "/photography-website/static/media/puppy.a1e77e09.jpg"
+  },
+  {
+    "revision": "2e6c8fdebbe09399d5c45d7e81a5c8c3",
+    "url": "/photography-website/static/media/sideImage.2e6c8fde.jpg"
+  },
+  {
+    "revision": "816453e67371a3dddd7857a11ed2bd16",
+    "url": "/photography-website/static/media/sydney2.816453e6.jpg"
+  },
+  {
+    "revision": "3228e3020ff800ed66b95973535d2355",
+    "url": "/photography-website/static/media/tripod.3228e302.jpg"
+  },
+  {
+    "revision": "8e3ef07b4b5aac0b0f54989c744921a0",
+    "url": "/photography-website/static/media/undraw_camera.8e3ef07b.png"
+  }
+]);
